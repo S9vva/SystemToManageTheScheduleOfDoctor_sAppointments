ec4abc7464a4a1440dc91452ab0981251c8877d9
andri <andri@ANDRII>
2024-10-15 12:22:11 +0200

    Dodaj pliki projektów.

---
+++ SystemToManageTheScheduleOfDoctor_sAppointments/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SystemToManageTheScheduleOfDoctor_sAppointments
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection sqlConnection = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Btn_Click_Two(object sender, RoutedEventArgs e)
+        {
+            Register r1  = new Register();
+            this.Close();
+            r1.Show();
+        }
+
+        private void Login_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SDoctor_sDB"].ConnectionString);
+            sqlConnection.Open();
+
+            string add_data = "SELECT * FROM [dbo].[Client] WHERE username=@username AND password=@password";
+            SqlCommand cmd = new SqlCommand(add_data, sqlConnection);
+
+            cmd.Parameters.AddWithValue("@username", username.Text);
+            cmd.Parameters.AddWithValue("@password", password.Password);
+            cmd.ExecuteNonQuery();
+            int Count = Convert.ToInt32(cmd.ExecuteScalar());
+
+            sqlConnection.Close();
+            username.Text = string.Empty;
+            password.Password = string.Empty;
+            if (Count > 0)
+            {
+                MainClient mainClient = new MainClient();
+                this.Close();
+                mainClient.Show();
+            }
+            else
+            {
+                MessageBox.Show("Password or Username is not correct");
+            }
+        }
+    }
+}
---
+++ SystemToManageTheScheduleOfDoctor_sAppointments/Register.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+
+namespace SystemToManageTheScheduleOfDoctor_sAppointments
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        private SqlConnection sqlConnection = null;
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SDoctor_sDB"].ConnectionString);
+                sqlConnection.Open();
+
+                string add_data = "INSERT INTO [dbo].[Client] VALUES(@username, @password)";
+                SqlCommand cmd = new SqlCommand(add_data, sqlConnection);
+
+                cmd.Parameters.AddWithValue("@username", username.Text);
+                cmd.Parameters.AddWithValue("@password", password.Password);
+                cmd.ExecuteNonQuery();
+                sqlConnection.Close();
+                username.Text = string.Empty;
+                password.Password = string.Empty;
+                MainWindow w1 = new MainWindow();
+                this.Close();
+                w1.Show();
+            }
+
+            catch
+            { 
+
+            }
+        }
+    }
+}
